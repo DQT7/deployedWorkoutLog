d004fe3a4cc88fa1641e6791566af764f43e6b2e
DeQuon Turner <dqturner@gmail.com>
2018-02-02 10:51:45 -0500

    added files for deployment

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = ('gulp');
+var concat = require('gulp-concat');
+var uglify = reqire('gulp-uglify');
+
+var javascriptFiles = [
+    './app.js',
+    './workouts/define.js',
+    './workouts/log.js',
+    './user/auth.js'
+];
+
+gulp.task('bundle', function(){
+    return gulp.search(javascriptFiles)
+    .pipe(concat('bundle.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist"));
+
+
+});
+gulp.task('default', ['bundle']);
